2b0c848a9faa64d9e2d26e254f0b92252f155388
wangyu1021 <wangyuyun1021@163.com>
2019-10-31 11:04:24 +0800

    FD

---
+++ FD/src/main/java/com/fd/controller/PageController.java
@@ -0,0 +1,16 @@
+package com.fd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class PageController {
+	
+	@RequestMapping("/index")
+	@ResponseBody
+	public String index() {
+		return "index";
+	}
+}
